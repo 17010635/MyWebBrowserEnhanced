bcd6d7cf4474374fc2f7af6a094a2ef62d168e98
17010635 <17010635@myrp.edu.sg>
2018-08-08 11:05:18 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/mywebbrowser/MainActivity.java
@@ -0,0 +1,36 @@
+package sg.edu.rp.c346.mywebbrowser;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etURL;
+    Button btnLoadURL;
+    WebView wvMyPage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnLoadURL = findViewById(R.id.buttonLoad);
+        wvMyPage = findViewById(R.id.webViewMyPage);
+        etURL = findViewById(R.id.editTextURL);
+
+        wvMyPage.setWebViewClient(new WebViewClient());
+
+        btnLoadURL.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String url = etURL.getText().toString();
+                wvMyPage.loadUrl(url);
+            }
+        });
+    }
+}
